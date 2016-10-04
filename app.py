6f3133619499541ade4c4f0ae9bb27d5c40d694f
Celine Yan <celine@Celines-iMac.home>
2016-10-03 23:50:32 -0400

    THIS WAS VERY BRUTAL BUT AT LEAST IT WORKS.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #9/28
 from flask import Flask, render_template, request
-from utils.authreader import readIn, dic, availAlready, makeAcc
+from utils.authreader import readIn, dic, availAlready, checkAcc, makeAcc
 import hashlib
 
 app = Flask(__name__)
+
+
 
 @app.route("/")
 @app.route("/login/")
@@ -11,20 +13,38 @@ def login():
     print request.headers
     return render_template('login.html', message = "")
 
-@app.route("/authenticate/", methods = ['POST'])
-def auth():
-   print request.headers
-   print request.form
-   user = "hello"
-   pw = "itsme"
-   auth = (request.form['user'] == user and request.form['password'] == pw)
-   #return render_template('login.html', message = auth)
-   return render_template('loginstat.html', auth = auth)
+
 
 @app.route("/register/")
 def register():
 	print request.headers
 	return render_template('register.html', message = "")
+
+
+
+@app.route("/authenticate/", methods = ['POST'])
+def auth():
+   print request.headers
+   print request.form
+
+   user = request.form['user'] 
+   pw = request.form['password']
+
+   if user == "" or pw == "": #CHECKING FOR STUPID ACTS
+   	return render_template('login.html', message = "Both fields must be filled out.")
+   	
+   pw = hashlib.sha224(pw).hexdigest() #hashed 
+
+   readIn()
+   if availAlready(user,pw) == False:
+   	return render_template('loginstat.html', auth = False, message = "This username does not exist.")
+   elif availAlready(user,pw):
+   	if checkAcc(user,pw) == False:
+   		return render_template('loginstat.html', auth = False, message = "Your password is incorrect.")
+
+   return render_template('loginstat.html', auth = True)
+
+
 
 @app.route("/rauthenticate/", methods = ['POST'])
 def rauth():
@@ -33,6 +53,10 @@ def rauth():
 
    user = request.form['user'] 
    pw = request.form['password']
+
+   if user == "" or pw == "": #CHECKING FOR STUPID ACTS
+   	return render_template('register.html', message = "Please fill in both fields.")
+
    pw = hashlib.sha224(pw).hexdigest() #hash it!!!!
 
    readIn()
@@ -44,9 +68,8 @@ def rauth():
    	makeAcc(user,pw)
    	print dic
 
-   #auth = (request.form['user'] == user and request.form['password'] == pw)
-   #return render_template('loginstat.html', auth = auth)
    return render_template('login.html', message = "Your account was successfully made! Login below.")
+
 
 
 if __name__ == "__main__":
--- utils/authreader.py
+++ utils/authreader.py
@@ -18,27 +18,33 @@ def availAlready(x,y): # in auth.csv or na
 		return False
 
 # ================================================ login
-
-
+def checkAcc(x,y):
+	readIn()
+	if dic[x] != y: #not in auth.csv
+		return False
+	elif dic[x] == y: #it's right!!
+		return True
 
 # ================================================ register
-def availAlready(x,y):
-	if x in dic:
-		return True
-	if x not in dic:
-		#makeAcc(x,y)
-		return False
-
 def makeAcc(x,y):
-
 	with open('data/auth.csv', 'ab') as csvfile: # APPEND
 		fieldnames = ['User', 'Password']
 		writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
 		writer.writerow({'User': x, 'Password': y })
 
-	readIn()
+
+
+
+
+
+
+
+
+
 
 '''
+FOR FUTURE REFERENCES:
+
 # ================================================ read in csv
 with open('data/auth.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
