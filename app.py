b3ca1e0a4ce2700caab9dec512eb235bfa969bad
Celine Yan <celine@Celines-iMac.home>
2016-10-12 17:21:15 -0400

    a space took away 4 hours of my life

--- app.py
+++ app.py
@@ -13,64 +13,42 @@ def login():
     print request.headers
     return render_template('login.html', message = "")
 
-
-
-@app.route("/register/")
-def register():
-	print request.headers
-	return render_template('register.html', message = "")
-
-
-
-@app.route("/authenticate/", methods = ['POST'])
+@app.route("/authenticate/", methods = ['GET','POST'])
 def auth():
    print request.headers
    print request.form
 
    user = request.form['user'] 
    pw = request.form['password']
+   pw = hashlib.sha224(pw).hexdigest() #hashed
+   stat = request.form['account']
 
-   if user == "" or pw == "": #CHECKING FOR STUPID ACTS
-   	return render_template('login.html', message = "Both fields must be filled out.")
+   if stat == 'Login':
    	
-   pw = hashlib.sha224(pw).hexdigest() #hashed 
-
-   readIn()
-   if availAlready(user,pw) == False:
-   	return render_template('loginstat.html', auth = False, message = "This username does not exist.")
-   elif availAlready(user,pw):
-   	if checkAcc(user,pw) == False:
-   		return render_template('loginstat.html', auth = False, message = "Your password is incorrect.")
-
-   return render_template('loginstat.html', auth = True)
-
-
-
-@app.route("/rauthenticate/", methods = ['POST'])
-def rauth():
-   print request.headers
-   print request.form
-
-   user = request.form['user'] 
-   pw = request.form['password']
-
-   if user == "" or pw == "": #CHECKING FOR STUPID ACTS
-   	return render_template('register.html', message = "Please fill in both fields.")
-
-   pw = hashlib.sha224(pw).hexdigest() #hash it!!!!
-
-   readIn()
-   if availAlready(user,pw):
-   	return render_template('register.html', message = "That username already exists. Please use another one.")
-
-   elif availAlready(user,pw) == False:
-   	print availAlready(user,pw)
-   	makeAcc(user,pw)
-   	print dic
-
-   return render_template('login.html', message = "Your account was successfully made! Login below.")
-
-
+       if user == "" or pw == "": #CHECKING FOR STUPID ACTS
+           return render_template('login.html', message = "Both fields must be filled out.")
+       readIn()
+       if availAlready(user,pw) == False:
+       	return render_template('loginstat.html', auth = False, message = "This username does not exist.")
+       elif availAlready(user,pw):
+       	if checkAcc(user,pw) == False:
+       		return render_template('loginstat.html', auth = False, message = "Your password is incorrect.")
+       		
+       return render_template('loginstat.html', auth = True)
+        
+   elif stat == 'Register':
+    	
+        if user == "" or pw == "": #CHECKING FOR STUPID ACTS
+            return render_template('login.html', message = "Please fill in both fields.")
+        readIn()
+        if availAlready(user,pw):
+            return render_template('login.html', message = "That username already exists. Please use another one.")
+        elif availAlready(user,pw) == False:
+            print availAlready(user,pw)
+            makeAcc(user,pw)
+            print dic
+            
+        return render_template('login.html', message = "Your account was successfully made! Login below.")
 
 if __name__ == "__main__":
     app.debug = True
